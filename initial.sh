080b7f2d2e50f7b724100571088660ae6078e246
Sharan Velusamy <sharan@mallow-tech.com>
2021-04-10 15:09:54 +0530

    initial commit 20

--- initial.sh
+++ initial.sh
@@ -1,12 +1,12 @@
-echo -e "\nBegin Transaction...\n"
+echo -e "\nBegin Transaction..."
 # Alias Files 
 echo -e "$(curl -fsSL https://raw.githubusercontent.com/sharanvelu/eb-command/master/.sharan-own-aliases)" >> ~/.sharan_own_aliases
 echo -e "$(curl -fsSL https://raw.githubusercontent.com/sharanvelu/eb-command/master/.sharan-bash-aliases)" >> ~/.sharan_bash_aliases
 
-if grep -q ". ~/.sharan_bash_aliases" ~/.bashrc; then
+if ! grep -q ". ~/.sharan_bash_aliases" ~/.bashrc; then
 	echo ". ~/.sharan_bash_aliases" >> ~/.bashrc
 fi
-if grep -q ". ~/.sharan_own_aliases" ~/.bashrc; then
+if ! grep -q ". ~/.sharan_own_aliases" ~/.bashrc; then
 	echo ". ~/.sharan_own_aliases" >> ~/.bashrc
 fi
 
