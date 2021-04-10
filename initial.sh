539c374f0fba547244fdce6671dcf11c27dec988
Sharan Velusamy <sharan@mallow-tech.com>
2021-04-10 11:56:01 +0530

    initial commit 4

--- initial.sh
+++ initial.sh
@@ -1,13 +1,4 @@
 git clone https://github.com/sharanvelu/eb-command.git ~/.sharan/eb-command
 
-echo ""
-echo "Cloning Eb-Command Git Repository..."
+echo "\nCloned Eb-Command Git Repository...\n"
 
-# Getting ENV variable into the EB instance
-echo ""
-echo "Getting ENV Variables"
-echo ""
-cd /var/app/current
-sudo chmod 777 /opt/elasticbeanstalk/deployment/env
-export $(xargs -a /opt/elasticbeanstalk/deployment/env)
-echo "Done exporting"
