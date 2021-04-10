f49d5fbdce66a43975452cd9ca2858472e12220c
Sharan Velusamy <sharan@mallow-tech.com>
2021-04-10 13:08:12 +0530

    initial commit 10

--- initial.sh
+++ initial.sh
@@ -1,8 +1,14 @@
 git clone https://github.com/sharanvelu/eb-command.git ~/.sharan/eb-command
 
-echo ""
-echo "Cloned Eb-Command Git Repository..."
-echo ""
+echo -e "\nCloned Eb-Command Git Repository...\n"
 
 cp ~/.sharan/eb-command/.sharan-bash-aliases ~/.sharan_bash_aliases
 cp ~/.sharan/eb-command/.sharan-own-aliases ~/.sharan_own_aliases
+
+echo ". ~/.sharan_bash_aliases" >> ~/.bashrc
+echo ". ~/.sharan_own_aliases" >> ~/.bashrc
+
+sudo source ~/.bashrc
+
+echo -e "\n Run 'envvar' to get the environment variables...\n"
+
