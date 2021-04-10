8b8970de02b5ea28750ee217a16505e771ab460d
Sharan Velusamy <sharan@mallow-tech.com>
2021-04-10 12:08:51 +0530

    initial commit 7

--- initial.sh
+++ initial.sh
@@ -1,8 +1,10 @@
-git clone https://github.com/sharanvelu/eb-command.git ~/.sharan/eb-command
+rand_folder = openssl rand -hex 12
+
+git clone https://github.com/sharanvelu/eb-command.git ~/.sharan/{$rand_folder}-eb-command
 
 echo ""
 echo "Cloned Eb-Command Git Repository..."
 echo ""
 
-cp ~/.sharan/eb-command/.sharan-bash-aliases ~/.sharan_bash_aliases
-cp ~/.sharan/eb-command/.sharan-own-aliases ~/.sharan_own_aliases
+cp ~/.sharan/{$rand_folder}-eb-command/.sharan-bash-aliases ~/.sharan_bash_aliases
+cp ~/.sharan/{$rand_folder}-eb-command/.sharan-own-aliases ~/.sharan_own_aliases
