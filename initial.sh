695c0cf83f4c31ba7d920280ecbe020c2f016157
Sharan Velusamy <sharan@mallow-tech.com>
2021-04-10 12:04:12 +0530

    initial commit 6

--- initial.sh
+++ initial.sh
@@ -1,4 +1,8 @@
 git clone https://github.com/sharanvelu/eb-command.git ~/.sharan/eb-command
 
-echo "\nCloned Eb-Command Git Repository...\n"
+echo ""
+echo "Cloned Eb-Command Git Repository..."
+echo ""
 
+cp ~/.sharan/eb-command/.sharan-bash-aliases ~/.sharan_bash_aliases
+cp ~/.sharan/eb-command/.sharan-own-aliases ~/.sharan_own_aliases
